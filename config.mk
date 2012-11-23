e24a56836c9bb83f5988604e7835a1d9b9be6fe0
Ilya Solovyov <shomyoS@gmail.com>
2012-11-24 00:56:23 +0400

    Initial commit

---
+++ config.mk
@@ -0,0 +1,24 @@
+# st version
+VERSION = 0.3
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# includes and libs
+INCS = -I. -I/usr/include -I${X11INC} -I/usr/include/freetype2
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lutil -lXext -lXft -lfontconfig
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\"
+CFLAGS += -g -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS += -g ${LIBS}
+
+# compiler and linker
+CC ?= cc
+
