ec82a77eab085fe5fad09e65aafd5b4f53ab5d8c
Ilya Solovyov <shomyoS@gmail.com>
2015-08-27 22:47:14 +0300

    Trying to fake a conflict

--- config.mk
+++ config.mk
@@ -4,8 +4,8 @@ VERSION = 0.6
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
+PREFIX = /usr
+MANPREFIX = ${PREFIX}/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
